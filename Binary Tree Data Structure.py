e60d4a2d8a2669b243950a5baecb3f343c1d2418
Eric Prehn <51291471+ericprehn@users.noreply.github.com>
2020-08-07 14:40:05 +0100

    Add files via upload
    Common Class for Binary Tree Data Structure

---
+++ Binary Tree Data Structure.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  6 15:18:20 2020
+
+@author: eric1
+"""
+   # just for the root:
+class Node:
+    
+    def __init__(self,data):
+        
+        self.left=None
+        self.right=None
+        self.data=data
+        
+    def insert(self,data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left=Node(data)
+                else:
+                    self.left.insert(data)
+            elif data >self.data:
+                if self.right is None:
+                    self.right=Node(data)
+                else:
+                    self.right.insert(data)
+                
+            
+                
+        
+        else:
+            self.data=data
+        
+    
+        
+    def Print(self):
+        if self.left:
+            self.left.Print()
+        print (self.data)
+        if self.right:
+            self.right.Print()
+        
+        
+
+root=Node(10)
+root.insert(12)
+root.insert(6)
+root.insert(3)
+root.insert(1)
+root.insert(2)
+
+root.Print()
